b3b46cbfb7442dbd8c35e8618ae393f4365e29f3
Casey Ann Cumbow <cumbow8@gmail.com>
2016-09-12 17:53:48 -0600

    Adds custom validation - too young

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,4 +1,8 @@
 class Horse < ApplicationRecord
   validates :name, :breed, :age, presence: true
   validate :too_young
+
+  def too_young
+    errors.add(:age, "Must be 4 years old") if age < 4
+  end
 end
