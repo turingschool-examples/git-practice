6d8abeab58206b4a4a4918ccd2d3a85438eadb3d
Rapha Barbo <rsbarbo@gmail.com>
2016-09-13 11:15:25 -0600

    WIP practicing with github (#11)
    WIP practicing with github - create route for items-index, and create items controller with index method, create view for index in our items controller, create an item mode, create item table, migrate item, schema update, create item index view, passes item in items controller.

--- app/models/stable.rb
+++ app/models/stable.rb
@@ -1,3 +1,8 @@
 class Stable < ApplicationRecord
   validates :location, presence: true
+
+  def something
+    #something does nothing other than adding
+    #info to my page to play with github functionalities.
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912233825) do
+ActiveRecord::Schema.define(version: 20160913165856) do
 
   create_table "horses", force: :cascade do |t|
     t.string   "name"
     t.string   "breed"
     t.integer  "age"
     t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "quantity"
+  end
+
+  create_table "stables", force: :cascade do |t|
+    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
