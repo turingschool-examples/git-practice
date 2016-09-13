83124bb3a7af295d717bb65f26f7c6d01d80e9b0
Casey Ann Cumbow <cumbow8@gmail.com>
2016-09-13 10:27:04 -0600

    Removes US validation

--- app/models/stable.rb
+++ app/models/stable.rb
@@ -1,15 +1,3 @@
 class Stable < ApplicationRecord
   validates :location, presence: true
-
-  validate :in_united_states
-
-  def in_united_states
-    errors.add(:location, "Must be in the US") unless location == "US"
-  end
-
-
-
-
-
-
 end
