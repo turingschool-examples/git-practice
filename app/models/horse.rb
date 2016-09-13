aebf53c28c0471e67f7e9fa46902380e60b5eba3
Casey Ann Cumbow <cumbow8@gmail.com>
2016-09-13 10:36:45 -0600

    remove space

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -2,7 +2,6 @@ class Horse < ApplicationRecord
   validates :name, :age, presence: true
   validate :older_than_four
 
-  
   def older_than_four
     errors.add(:age, 'Must be 4 years old') if age < 4
   end
