0e5d6cc069c2b9b4722e0043779935f8fac0da09
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-13 10:43:57 -0600

    Add item model and migration
    Add items index route
    
    Add items index view template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912233825) do
+ActiveRecord::Schema.define(version: 20160913164159) do
 
   create_table "horses", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20160912233825) do
     t.integer  "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
   end
 
 end
