c844feeb5ebcaa3b2e6ff579bb18e1037e7de464
Nate Anderson <curtislowe10@gmail.com>
2016-09-13 10:42:50 -0600

    adds Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912233825) do
+ActiveRecord::Schema.define(version: 20160913164138) do
 
   create_table "horses", force: :cascade do |t|
     t.string   "name"
