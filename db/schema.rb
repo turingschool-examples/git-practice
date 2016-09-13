dd1a1efd0c78620b2b6fdfae96d5220ba2432c5e
tgisg <soniagupta504@gmail.com>
2016-09-13 10:56:44 -0600

    adds name attribute to item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913164101) do
+ActiveRecord::Schema.define(version: 20160913165602) do
 
   create_table "horses", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160913164101) do
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
